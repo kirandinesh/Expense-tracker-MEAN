ab096e7d1bbb95c588819e1f28a0ebc607b84ea8
Kirandinesh <kirandinesh646@gmail.com>
2025-04-15 21:37:22 +0530

    new

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,1 +1,6 @@
-export const environment = {};
+export const environment = {
+  production: true,
+  authApiUrl: 'expense-tracker-mean-production.up.railway.app/auth',
+  expenseApiUrl: 'expense-tracker-mean-production.up.railway.app/expense',
+  categoryApiUrl: 'expense-tracker-mean-production.up.railway.app/category',
+};
